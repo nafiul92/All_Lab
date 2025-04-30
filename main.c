d194b9acc84ed51688840b001cb356bbfd5e4d9d
nafiul92 <157359319+nafiul92@users.noreply.github.com>
2025-04-30 22:07:49 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -3,26 +3,16 @@
 #define MAX_PAGES 100
 #define MAX_FRAMES 10
 
-int findLRU(int time[], int n) {
-    int i, minimum = time[0], pos = 0;
-    for (i = 1; i < n; i++) {
-        if (time[i] < minimum) {
-            minimum = time[i];
-            pos = i;
-        }
-    }
-    return pos;
-}
-
 int main() {
-    int pages[MAX_PAGES], frames[MAX_FRAMES], time[MAX_FRAMES];
-    int n_pages, n_frames, counter = 0, page_faults = 0;
-    int i, j, pos, hit;
+    int n_pages, n_frames;
+    int pages[MAX_PAGES], frames[MAX_FRAMES];
+    int i, j, pointer = 0, page_faults = 0;
+    int hit;
 
     printf("Enter number of pages: ");
     scanf("%d", &n_pages);
 
-    printf("Enter the page reference sequence:\n");
+    printf("Enter page reference sequence:\n");
     for (i = 0; i < n_pages; i++) {
         scanf("%d", &pages[i]);
     }
@@ -31,31 +21,25 @@ int main() {
     scanf("%d", &n_frames);
 
     for (i = 0; i < n_frames; i++) {
-        frames[i] = -1;
-        time[i] = 0;
+        frames[i] = -1;  /* initialize empty frames */
     }
 
-    printf("\nLRU Page Replacement:\n");
+    printf("\nFIFO Page Replacement:\n");
     printf("Reference | Frames\n");
     printf("----------+---------------------\n");
 
     for (i = 0; i < n_pages; i++) {
         hit = 0;
-
         for (j = 0; j < n_frames; j++) {
             if (frames[j] == pages[i]) {
-                counter++;
-                time[j] = counter;
                 hit = 1;
                 break;
             }
         }
 
         if (!hit) {
-            pos = findLRU(time, n_frames);
-            counter++;
-            frames[pos] = pages[i];
-            time[pos] = counter;
+            frames[pointer] = pages[i];
+            pointer = (pointer + 1) % n_frames;
             page_faults++;
         }
 
@@ -66,7 +50,10 @@ int main() {
             else
                 printf(" %3d", frames[j]);
         }
-        printf("%s\n", hit ? "  (HIT)" : "  (FAULT)");
+        if (hit)
+            printf("  (HIT)\n");
+        else
+            printf("  (FAULT)\n");
     }
 
     printf("----------------------------------\n");
